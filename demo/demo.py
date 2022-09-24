503422274e617b72e9c7c0a5cf9cf7951610c572
jjpenad <juanpd.26@gmail.com>
2022-09-24 04:23:59 -0500

    fix dtypes

--- demo/demo.py
+++ demo/demo.py
@@ -1,7 +1,6 @@
 import rasterio
 from random import randint
 
-
 def distort_image(source_path, target_path):
     with rasterio.open(source_path) as src:
         r = src.read()
@@ -13,7 +12,7 @@ def distort_image(source_path, target_path):
         new_dataset = rasterio.open(target_path,'w',driver= src.driver, height=src.shape[0],
             width=src.shape[1],
             count=src.count,
-            dtype='uint8',
+            dtype=src.dtypes[0],
             #nodata=255,
             crs=src.crs,
             transform=transform2,)
@@ -21,4 +20,5 @@ def distort_image(source_path, target_path):
         new_dataset.write(r)
         new_dataset.close()
 
-distort_image('images/coordinates.tif', 'images/newTif.tif')
+
+distort_image('images/mision79_linea2_1415_180801_151628_rgbnn00l2.img', 'images/newImg.img')
